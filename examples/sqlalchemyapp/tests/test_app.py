f3a9c603a38e368b19b4abd77d609ade68272c67
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2016-11-05 08:30:11 +0100

    more tests

--- examples/sqlalchemyapp/tests/test_app.py
+++ examples/sqlalchemyapp/tests/test_app.py
@@ -11,3 +11,21 @@ def test_add(app):
     todo_id = app.add('foo').entity_id
     todo = app.repo.get_entity(todo_id)
     assert todo.name == 'foo'
+    assert todo.done is False
+
+
+def test_finish(app):
+    todo_id = app.add('foo').entity_id
+    app.finish(todo_id)
+    todo = app.repo.get_entity(todo_id)
+    assert todo.name == 'foo'
+    assert todo.done is True
+
+
+def test_reopen(app):
+    todo_id = app.add('foo').entity_id
+    app.finish(todo_id)
+    app.reopen(todo_id)
+    todo = app.repo.get_entity(todo_id)
+    assert todo.name == 'foo'
+    assert todo.done is False

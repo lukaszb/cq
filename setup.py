f426cd388e59a4d96723ac9e930ecaba2c2d567d
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2017-03-30 06:42:12 +0200

    Merge from master

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_packages(top):
 
 setup(
     name='cq',
-    version='0.9.dev',
+    version='0.9dev',
     url='https://github.com/lukaszb/cq',
     license='MIT',
     description='simple cqrs implementation',

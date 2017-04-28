1f7d081c811b77869f61d0d360f897f2ea047b30
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2017-04-28 19:48:29 +0200

    added test for aggregate version bump

--- cq/__init__.py
+++ cq/__init__.py
@@ -1,1 +1,9 @@
+from . import aggregates
+from . import app
+from . import storages
+
+
+__all__ = ['aggregates', 'app', 'storages']
+
+
 default_app_config = 'cq.contrib.django.apps.SimpleEventSourcingApp'
--- cq/app.py
+++ cq/app.py
@@ -4,6 +4,16 @@ from . import settings
 from .aggregates import Repository
 
 
+def command(method):
+    method.is_command = True
+    return method
+
+
+def query(method):
+    method.is_query = True
+    return method
+
+
 class BaseApp:
     storage_class = None
     storage_kwargs = {}
@@ -34,3 +44,11 @@ class BaseApp:
         name = '%sRepository' % aggregate_class
         repo_class = type(name, (Repository,), {'aggregate_class': aggregate_class})
         return repo_class(storage=self.storage)
+
+    def get_commands(self):
+        objects = (getattr(self, attr) for attr in dir(self))
+        return [obj for obj in objects if getattr(obj, 'is_command', False)]
+
+    def get_queries(self):
+        objects = (getattr(self, attr) for attr in dir(self))
+        return [obj for obj in objects if getattr(obj, 'is_query', False)]

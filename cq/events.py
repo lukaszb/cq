e46fb7b082f26f0f72bc2b031af393d213a016fa
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2017-04-30 10:56:38 +0200

    added ability to upcast events - fixes #26

--- cq/events.py
+++ cq/events.py
@@ -1,14 +1,42 @@
+from collections import namedtuple
 import datetime
 
 
+upcaster = namedtuple('Upcaster', 'aggregate_type, event_name, revision, method')
+
+
 class Event:
-    def __init__(self, id, aggregate_type, name, aggregate_id, data=None, ts=None):
+    def __init__(self, id, aggregate_type, name, aggregate_id, data=None, ts=None, revision=1):
         self.id = id
         self.aggregate_type = aggregate_type
         self.name = name
         self.aggregate_id = aggregate_id
         self.data = data
         self.ts = ts or datetime.datetime.now()
+        self.revision = revision
 
     def __str__(self):
         return '%s.%s | %s | %s' % (self.aggregate_type, self.name, self.aggregate_id, self.ts)
+
+
+def upcast(event, upcasters):
+    event_upcasters = get_upcasters_for_event(event, upcasters)
+    for upcaster in event_upcasters:
+        single_upcast(event, upcaster)
+
+
+def single_upcast(event, upcaster):
+    upcaster.method(event)
+    event.revision += 1
+
+
+def get_upcasters_for_event(event, upcasters):
+
+    def matches_event(upcaster):
+        return (upcaster.aggregate_type == event.aggregate_type and
+                upcaster.event_name == event.name and
+                upcaster.revision >= event.revision)
+
+    relevant_upcasters = [upcaster for upcaster in upcasters if matches_event(upcaster)]
+    relevant_upcasters.sort(key=lambda upcaster: upcaster.revision)
+    return relevant_upcasters
---
+++ cq/tests/test_upcasting.py
@@ -0,0 +1,39 @@
+from cq import events
+from cq.genuuid import genuuid
+
+
+def test_event_upcast():
+
+    event = events.Event(
+        id=genuuid(),
+        aggregate_type='User',
+        name='Registered',
+        aggregate_id=genuuid(),
+        data={'email': 'joe@doe.com'},
+    )
+
+    assert event.revision == 1
+
+    def add_role(event):
+        event.data['role'] = 'user'
+
+    def add_fullname(event):
+        event.data['fullname'] = None
+
+    def noop(event):
+        pass
+
+    upcasters = [
+        events.upcaster('User', 'Registered', revision=1, method=add_role),
+        events.upcaster('User', 'Registered', revision=2, method=add_fullname),
+        events.upcaster('User', 'ChangedRole', revision=1, method=noop),
+    ]
+
+    events.upcast(event, upcasters)
+
+    assert event.revision == 3
+    assert event.data == {
+        'email': 'joe@doe.com',
+        'role': 'user',
+        'fullname': None,
+    }

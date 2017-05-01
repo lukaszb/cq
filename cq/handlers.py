f46e3d2dce5ada22fca2879b808138b82b0e2547
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2017-05-01 21:59:23 +0200

    better info on exception

--- cq/handlers.py
+++ cq/handlers.py
@@ -19,7 +19,8 @@ def check_handler_signature(handler):
     signature = inspect.signature(handler)
     params = list(signature.parameters.keys())
     if params != ['event', 'replaying_events']:
-        msg = "Handlers must accept 'event' and 'replaying_events' (%s does not)" % handler
+        handler_path = get_obj_path(handler)
+        msg = "Handlers must accept 'event' and 'replaying_events' (%s does not)" % handler_path
         print(" params: %s" % str(params))
         raise cq.exceptions.ImproperlyConfigured(msg)
 
@@ -33,3 +34,5 @@ def get_handlers(event):
     return handlers_registry[(event.aggregate_type, event.name)]
 
 
+def get_obj_path(obj):
+    return '%s.%s' % (obj.__module__, obj.__qualname__)

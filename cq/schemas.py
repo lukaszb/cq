a9837a5e6d7fef1a9c583a86b8f147759fbe790f
Piotr Duda <piotrekno1@gmail.com>
2017-08-02 01:11:53 +0200

    Fix Tests

--- cq/schemas.py
+++ cq/schemas.py
@@ -14,6 +14,7 @@ __all__ = ['EventSchema', 'fields']
 class EventSchema(_Schema):
     @validates_schema(pass_original=True)
     def check_unknown_fields(self, data, original_data):
+        original_data = original_data or {}
         unknown = set(original_data) - set(self.fields)
         if unknown:
             raise ValidationError('Unknown field', unknown)

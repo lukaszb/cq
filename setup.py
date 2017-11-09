a98c28ae37f3f16a11d4a8939a38d19f14a05329
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2017-11-09 13:57:15 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_packages(top):
 
 setup(
     name='cq',
-    version='0.11',
+    version='0.12dev',
     url='https://github.com/lukaszb/cq',
     license='MIT',
     description='simple cqrs implementation',
